1f326e14c63e86cc988f8b115b5ebec499c19000
Bhuvansh Goyal <bhuvanshgoyal2004@gmail.com>
2023-03-28 01:22:35 +0530

    Initial commit

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ from helper import send_mails_to_all
 
 
 FONT = ("Calibri", 25, 'normal')
+RADIUS = 2
 
 
 class App(ctk.CTk):
@@ -18,40 +19,41 @@ class App(ctk.CTk):
         self.current_doc = None
         self.current_excel = None
 
-        self.container = ctk.CTkFrame(master=self)
-        self.container.grid(row=0, column=0, padx=40, pady=40)
+        self.container = ctk.CTkFrame(master=self, corner_radius=RADIUS)
+        self.container.grid(row=0, column=0, padx=10, pady=10)
 
-        self.add_files_button = ctk.CTkButton(master=self.container, width=150, font=FONT, text='Add files', command=self.add_files)
+        self.add_files_button = ctk.CTkButton(master=self.container, fg_color='#5d5d5d', hover_color='#4a4a4a', corner_radius=RADIUS, width=150, font=FONT, text='Add files', command=self.add_files)
         self.add_files_button.grid(row=2, column=0, padx=30, pady=20)
 
         self.files_label = ctk.CTkLabel(master=self.container, font=(FONT[0], 16, FONT[2]))
 
-        self.send_button = ctk.CTkButton(master=self.container, width=150, font=FONT, text='Send', state='disabled', command=self.send_mails)
+        self.send_button = ctk.CTkButton(master=self.container, fg_color='#5d5d5d', hover_color='#4a4a4a', corner_radius=RADIUS, width=150, font=FONT, text='Send', state='disabled', command=self.send_mails)
         self.send_button.grid(row=2, column=1, padx=30, pady=20)
 
     def send_mails(self):
         self.files_label.grid_remove()
         self.send_button.configure(state='disabled')
         self.add_files_button.configure(state='disabled')
-        self.container.grid_remove()
-
-        time.sleep(2)
 
         with open('data.txt') as file:
             lines = file.readlines()
             email = lines[0]
             password = lines[1]
 
-        send_mails_to_all(
+        unsent_messages = send_mails_to_all(
             doc_file=self.current_doc,
             excel_file=self.current_excel,
             sender=email,
             password=password,
         )
 
+        if not unsent_messages:
+            messagebox.showinfo(message='All messages sent')
+        else:
+            text = '\n'.join(unsent_messages)
+            messagebox.showinfo(message=text)
+
         self.add_files_button.configure(state='normal')
-        self.container.grid(row=0, column=0, padx=40, pady=40)
-        # show popup for done
 
     def add_files(self):
         self.current_doc = filedialog.askopenfilename(
